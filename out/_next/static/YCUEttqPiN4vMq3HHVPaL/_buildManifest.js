0bdeb684543bb9dc69d5840464ce4c00f9125608
MBSA-INFINITY <mbsaiaditya@gmail.com>
2023-10-31 18:49:32 +0000

    Deploy to mbsa branch

---
+++ out/_next/static/YCUEttqPiN4vMq3HHVPaL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-12d4adb491bab395.js"],"/_error":["static/chunks/pages/_error-b646007f40c4f0a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
